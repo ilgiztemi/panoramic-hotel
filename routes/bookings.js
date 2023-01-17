4de18b10abe1f379a12dd1ffd270c049b4bc371b
Kanykey Albanova <temirbekov91@gmail.com>
2023-01-17 09:56:56 -0500

    config dotenv file was created

--- routes/bookings.js
+++ routes/bookings.js
@@ -8,10 +8,6 @@ const recordRoutes = express.Router();
 // This will help us connect to the database
 const dbo = require("../db/conn");
  
-// This help convert the id from string to ObjectId for the _id.
-const ObjectId = require("mongodb").ObjectId;
- 
- 
 // This section will help you get a list of all the records.
 recordRoutes.route("/bookings").get(function (req, res) {
  let db_connect = dbo.getDb("panoramic-hotel");
