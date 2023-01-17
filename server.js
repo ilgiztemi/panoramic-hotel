0fd14cbd4f76b755d104137a98e9f563b2d70575
Kanykey Albanova <temirbekov91@gmail.com>
2023-01-17 09:46:14 -0500

    server file updated and connection to daabese was edited

--- server.js
+++ server.js
@@ -1,28 +1,19 @@
-const express = require( "express" );
-const morgan = require( "morgan" );
-const cors = require( "cors" );
-
-const PORT = 8888;
-
-express()
-  .use(cors({    
-    origin: [""],
-}))
-  .use(function (req, res, next) {
-    res.header(
-      "Access-Control-Allow-Methods",
-      "OPTIONS, HEAD, GET, PUT, POST, DELETE"
-    );
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept"
-    );
-    next();
-  })
-  .use(morgan("tiny"))
-  .use(express.json())
-  .use(express.urlencoded({ extended: false }))
-  .use("/", express.static(__dirname + "/"))
-  .use(require("./routes/bookingsRoutes"))
-
-  // .listen(PORT, () => console.log(`Listening on port ${PORT}`));
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config({ path: "./config.env" });
+const port = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+app.use(require("./routes/bookings"));
+// get driver connection
+const dbo = require("./db/conn");
+ 
+app.listen(port, () => {
+  // perform a database connection when server starts
+  dbo.connectToServer(function (err) {
+    if (err) console.error(err);
+ 
+  });
+  console.log(`Server is running on port: ${port}`);
+});
